fa1f1f435286f7275b996151b7bb9e2a5ebaa7d4
Tigran <tigran2589@gmail.com>
2022-07-11 19:16:28 +0300

    Created and added ErrorIndicator  component

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -4,6 +4,7 @@ import Spinner from '../Spinner/Spinner';
 import SwapiService from '../../services/swapi-service';
 
 import './RandomPlanet.css';
+import ErrorIndicator from '../ErrorIndicator/ErrorIndicator';
 
 export default class RandomPlanet extends Component {
 
@@ -11,7 +12,8 @@ export default class RandomPlanet extends Component {
 
   state = {
     planet: {},
-    loading: true
+    loading: true,
+    error: false
   };
 
   constructor() {
@@ -22,24 +24,39 @@ export default class RandomPlanet extends Component {
   onPlanetLoaded = (planet) => {
     this.setState({
       planet,
+      loading: false,
+      error: false
+    })
+  }
+
+  onError = (err) => {
+    this.setState({
+      error: true,
       loading: false
-    });
-  };
+    })
+  }
 
   updatePlanet() {
-    const id = Math.floor(Math.random() * 25) + 2;
+    //const id = Math.floor(Math.random() * 25) + 2
+    const id = 15
     this.swapiService
       .getPlanet(id)
-      .then(this.onPlanetLoaded);
+      .then(this.onPlanetLoaded)
+      .catch(this.onError)
   }
 
   render() {
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
+
+    const hasData = !(loading || error)
+
+    const errorMessage = error ? <ErrorIndicator /> : null
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet} /> : null;
+    const content = hasData ? <PlanetView planet={planet} /> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>
--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ import App from './components/App/App';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
+
+//https://github.com/Juriy/pro-react-redux/tree/master/star-db/100-higher-order-components/src/components
+//https://swapi.dev/
